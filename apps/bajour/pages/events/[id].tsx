0bee52895d584adfa998d2fb989c9fe80223482c
Karl Merkli <karlmerkli@gmail.com>
2023-05-16 11:40:38 +0200

    fix: use SSR for events for sharing

--- apps/bajour/pages/events/[id].tsx
+++ apps/bajour/pages/events/[id].tsx
@@ -1,21 +1,51 @@
-import {Container as MuiContainer, styled} from '@mui/material'
-import {EventContainer} from '@wepublish/website'
-import {useRouter} from 'next/router'
+import { Container as MuiContainer, styled } from '@mui/material';
+import { ApiV1, EventContainer, EventSEO } from '@wepublish/website';
+import { GetServerSideProps } from 'next';
+import getConfig from 'next/config';
+import { useRouter } from 'next/router';
 
 const Container = styled(MuiContainer)`
-  padding: ${({theme}) => theme.spacing(5)};
-`
+  padding: ${({ theme }) => theme.spacing(5)};
+`;
 
-export default function EventById() {
+type EventByIdProps = {
+  event?: ApiV1.Event;
+};
+
+export default function EventById({ event }: EventByIdProps) {
   const {
-    query: {id}
-  } = useRouter()
+    query: { id },
+  } = useRouter();
 
   return (
     <Container maxWidth="md" fixed>
       <main>
+        {event && <EventSEO event={event} />}
         <EventContainer id={id as string} />
       </main>
     </Container>
-  )
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { id } = params || {};
+
+  const { publicRuntimeConfig } = getConfig();
+  const client = ApiV1.getV1ApiClient(publicRuntimeConfig.env.API_URL!, []);
+  const data = await client.query({
+    query: ApiV1.EventDocument,
+    variables: {
+      id,
+    },
+  });
+
+  if (data.data.event) {
+    return {
+      props: { event: data.data.event },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
--- apps/bajour/pages/index.tsx
+++ apps/bajour/pages/index.tsx
@@ -1,14 +1,18 @@
-import {ApiV1, PageContainer} from '@wepublish/website'
-import {BajourEventList} from '../src/components/bajour-event-list'
+import { ApiV1 } from '@wepublish/website';
+import { BajourEventList } from '../src/components/bajour-event-list';
 
 export function Index() {
   return (
     <>
       <BajourEventList
-        variables={{filter: {upcomingOnly: false}, take: 100, sort: ApiV1.EventSort.StartsAt}}
+        variables={{
+          filter: { upcomingOnly: true },
+          take: 100,
+          sort: ApiV1.EventSort.StartsAt,
+        }}
       />
     </>
-  )
+  );
 }
 
-export default Index
+export default Index;
